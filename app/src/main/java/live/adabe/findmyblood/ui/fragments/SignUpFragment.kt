a94b4e6c7880df42a8eee7e72c63f4d884ac2e00
kelienzo <kellyefone@gmail.com>
2021-08-20 10:58:06 +0100

    implementations commit

--- app/src/main/java/live/adabe/findmyblood/ui/fragments/SignUpFragment.kt
+++ app/src/main/java/live/adabe/findmyblood/ui/fragments/SignUpFragment.kt
@@ -5,13 +5,18 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.Navigation
 import live.adabe.findmyblood.R
 import live.adabe.findmyblood.databinding.FragmentSignUpBinding
+import live.adabe.findmyblood.models.network.SignUpRequest
+import live.adabe.findmyblood.viewmodels.AuthViewModel
 
 class SignUpFragment : Fragment() {
 
     private lateinit var binding: FragmentSignUpBinding
+    private lateinit var viewModel: AuthViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -20,10 +25,47 @@ class SignUpFragment : Fragment() {
         // Inflate the layout for this fragment
         binding = FragmentSignUpBinding.inflate(inflater, container, false)
 
-        binding.tvLogin.setOnClickListener {
-            Navigation.findNavController(it).navigate(R.id.action_signUpFragment_to_loginFragment)
+        //viewModel = ViewModelProvider(this)[AuthViewModel::class.java]
+
+        binding.apply {
+
+            btnRegister.setOnClickListener {
+                addNewHospital()
+            }
+
+            tvLogin.setOnClickListener {
+                Navigation.findNavController(it)
+                    .navigate(R.id.action_signUpFragment_to_loginFragment)
+            }
         }
 
         return binding.root
+    }
+
+    private fun addNewHospital() {
+        binding.apply {
+            val nameOfHospital = textInputHospital.editText?.text.toString()
+            val email = textInputEmail.editText?.text.toString()
+            val password = textInputPassword.editText?.text.toString()
+            val confirmPassword = textInputConfirmPassword.editText?.text.toString()
+
+            if (inputCheck(nameOfHospital, email, password, confirmPassword)) {
+                val signUpRequest = SignUpRequest(nameOfHospital, email, password, confirmPassword)
+
+                //viewModel.signUpHospital(signUpRequest)
+            } else {
+                Toast.makeText(requireActivity(), "Please fill out all fields", Toast.LENGTH_LONG)
+                    .show()
+            }
+        }
+    }
+
+    private fun inputCheck(
+        nameOfHospital: String,
+        email: String,
+        password: String,
+        confirmPassword: String
+    ): Boolean {
+        return nameOfHospital.isNotEmpty() && email.isNotEmpty() && password.isNotEmpty() && confirmPassword.isNotEmpty()
     }
 }
