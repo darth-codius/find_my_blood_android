424e42b7531025084f5bfc21dff390faca10fab4
kelienzo <kellyefone@gmail.com>
2021-08-26 22:29:54 +0100

    Newest updates

--- app/src/main/java/live/adabe/findmyblood/ui/fragments/RequestScreenFragment.kt
+++ app/src/main/java/live/adabe/findmyblood/ui/fragments/RequestScreenFragment.kt
@@ -5,16 +5,36 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
 import live.adabe.findmyblood.R
+import live.adabe.findmyblood.databinding.FragmentRequestScreenBinding
 
 class RequestScreenFragment : Fragment() {
+    private lateinit var binding: FragmentRequestScreenBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_request_screen, container, false)
-    }
+        binding = FragmentRequestScreenBinding.inflate(inflater, container, false)
 
+        binding.spBloodGroup.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(
+                adapterView: AdapterView<*>?,
+                view: View?,
+                position: Int,
+                id: Long
+            ) {
+
+            }
+
+            override fun onNothingSelected(p0: AdapterView<*>?) {
+
+            }
+
+        }
+
+        return binding.root
+    }
 }
